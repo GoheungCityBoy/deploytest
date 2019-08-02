20c65f6c15f56b075fc7afbff39aafceae38308e
seungwon <deutschlandee@gmail.com>
2019-08-02 20:23:39 +0900

    Second

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\susul\onedrive\바탕 화면\likelion\4blog\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
